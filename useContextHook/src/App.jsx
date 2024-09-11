ad100b27836673a958588cfbfcf957be4cc1377e
02Anmol <tyagianmol839@gmail.com>
2024-09-11 21:37:54 +0530

    useContext Hook
    add use of useContext Hook in react

---
+++ useContextHook/src/App.jsx
@@ -0,0 +1,18 @@
+// this is use to manage global data in react app
+
+import React from 'react'
+import './App.css'
+import Profile from './Components/Profile'
+import Footer from './Components/Footer'
+
+function App() {
+  return (
+    <div>
+      <Profile/>
+      <Footer/>
+    </div>
+  )
+}
+
+export default App
+
